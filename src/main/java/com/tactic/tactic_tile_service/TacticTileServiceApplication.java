6c2a89f9e34382787a57dacec439f8c056f115ae
kapich1192 <kapich1192@yandex.ru>
2023-10-23 12:36:18 +0300

    First commit

---
+++ src/main/java/com/tactic/tactic_tile_service/TacticTileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tactic.tactic_tile_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacticTileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacticTileServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tactic/tactic_tile_service/TacticTileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tactic.tactic_tile_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacticTileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
