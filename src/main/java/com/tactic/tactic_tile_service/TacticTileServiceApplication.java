209ec643bbe5305731dbfa9800602ad1bfc6e0c4
kapich1192 <kapich1192@yandex.ru>
2023-10-23 20:58:50 +0300

    Добавил механику конфигурирования сервиса

--- src/main/java/com/tactic/tactic_tile_service/TacticTileServiceApplication.java
+++ src/main/java/com/tactic/tactic_tile_service/TacticTileServiceApplication.java
@@ -1,12 +1,18 @@
 package com.tactic.tactic_tile_service;
 
+import com.tactic.tactic_tile_service.models.elements.Element;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static com.tactic.tactic_tile_service.config.Singleton.thisElement;
 
 @SpringBootApplication
 public class TacticTileServiceApplication {
 
 	public static void main(String[] args) {
+		thisElement = new Element();
+		thisElement.initialize();
+
 		SpringApplication.run(TacticTileServiceApplication.class, args);
 	}
 
---
+++ src/main/java/com/tactic/tactic_tile_service/config/Singleton.java
@@ -0,0 +1,7 @@
+package com.tactic.tactic_tile_service.config;
+
+import com.tactic.tactic_tile_service.models.elements.Element;
+
+public class Singleton {
+    public static Element thisElement = null;
+}
