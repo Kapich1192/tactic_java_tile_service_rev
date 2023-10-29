f53a951c7d02000dc7f95910545fc91bfecce5f0
kapich1192 <kapich1192@yandex.ru>
2023-10-29 18:58:10 +0300

    fix: Изменил цвет окантовки в хедере

--- src/test/java/com/tactic/tactic_tile_service/TacticTileServiceApplicationTests.java
+++ src/test/java/com/tactic/tactic_tile_service/TacticTileServiceApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TacticTileServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void test1() {
+
+    }
 
 }
